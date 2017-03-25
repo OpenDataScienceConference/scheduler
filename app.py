37dd665501bc727b85565d3c12b8cf36e2d9836a
gfleetwood <gfleetwood@protonmail.com>
2017-03-25 08:33:32 -0400

    Fix indentation

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def transform_view():
     response.headers["Content-Disposition"] = "attachment; filename=odsc_east.json"
     return response
 
- if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
